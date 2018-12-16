86dd3ad2e55f90bce40aa0f74ac3f2f6e95922f8
Roland Levy <rolandjlevy@gmail.com>
2018-12-16 22:25:52 +0000

    Functionality working now

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 ## Technical test
 Following telephone interview on Thu 13 Dec 2018. Started on Sat 15 Dec
+
+### API documentation: 
+- https://randomuser.me/documentation#howto
+
+### Animation: 
+- https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
+- https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/translateX
+- https://css-tricks.com/ease-out-in-ease-in-out/
+- https://hackernoon.com/how-to-create-fancy-revealing-animations-with-these-simple-css-tricks-5b34614ae69a
+
+### Modal
+- https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal
+- https://www.viget.com/articles/the-dialog-element/
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 require('./base.scss');
 
-// documentation: https://randomuser.me/documentation#howto
 const baseUrl = "https://randomuser.me/api/";
-const contentNode = document.querySelector(".list");
+const usersList = document.querySelector(".users-list");
 const nationalityForm = document.querySelector(".nationality-form");
 const searchForm = document.querySelector(".search-form");
 const mainInfo = document.querySelector(".content__main-info");
+const dialog = document.querySelector('#dialog');
 let users = [];
 
-contentNode.addEventListener('click', event => {	
+usersList.addEventListener('click', event => {	
     event.preventDefault();
     if (event.target.matches('.user')){
         selectUser(event.target.id);
+        animateUser();
+        // dialog.className = 'dialog-show';
     }
 });
 
@@ -25,51 +27,47 @@ searchForm.addEventListener('submit', event => {
     fetchUsersSearch (event.target[0].value);
 });
 
+function fetchUsersNatMenu (nat){
+    const nationality = nat === "any" ? `` : `&nat=${nat}`;
+    const url = `${baseUrl}?inc=name,location,cell,email,picture,id,dob&results=20${nationality}`;
+    usersList.innerHTML = "Loading...";
+    return fetch(url)
+    .then(response => response.json())
+    .then(body => {
+        users = body.results;
+        usersList.innerHTML = displayUsers(users);
+    }).catch(error => console.log(error));
+}
+
+fetchUsersNatMenu("AU");
+
 function fetchUsersSearch (name){
-    const url = `${baseUrl}?inc=name,location,cell,email,picture,id,dob&results=1000`;
-    contentNode.innerHTML = `Checking for "${name}" in 5,000 records...`;
+    const url = `${baseUrl}?inc=name,location,cell,email,picture,id,dob&results=5000`;
+    usersList.innerHTML = `Checking for "${name}" in 5,000 records...`;
     return fetch(url)
     .then(response => response.json())
     .then(body => {
         users = body.results.filter(item => {
             return item.name.first.includes(name.toLowerCase()) || item.name.last.includes(name.toLowerCase())
         });
-        contentNode.innerHTML = displayUsers(users, name);
+        const slicedUsers = users.slice(0, 20);
+        usersList.innerHTML = displayUsers(slicedUsers);
+        nationalityForm[0].selectedIndex = 0;
     }).catch(error => console.log(error));
 }
 
-function fetchUsersNatMenu (nat){
-    if (nat === "none") return;
-    const url = `${baseUrl}?inc=name,location,cell,email,picture,id,dob&nat=${nat}&results=20`;
-    contentNode.innerHTML = "Loading...";
-    return fetch(url)
-    .then(response => response.json())
-    .then(body => {
-        users = body.results;
-        contentNode.innerHTML = displayUsers(users);
-    }).catch(error => console.log(error));
-}
-
-function displayUsers (arr, name='') {
+function displayUsers (arr) {
     return arr.map((item, index) => {
         let str = ``;
         str += (index === 0 || index % 2 === 0) ? `<article class="row">` : ``;
         str += 
         `<div class="column" href="#">
             <img id="${index}" class="user" src="${item.picture.medium}">
-            <div id="${index}" class="column__username user">${highlight(item.name.first, name)} ${highlight(item.name.last, name)}</div>
+            <div id="${index}" class="column__username user">${sentenceCase(item.name.first)} ${sentenceCase(item.name.last)}</div>
         </div>`;
         str += (index === 0 || index % 2 === 0) ? `` : `</article>`;
-        // highlight(item.name.first, name);
         return str;
     }).join('');
-}
-
-// highlights matched string from search
-function highlight (string, chunk) {
-    const str = string.toLowerCase();
-    const ch = chunk.toLowerCase();
-    return str.includes(ch) ? sentenceCase(string.replace(chunk, `<strong>${chunk}</strong>`)) : sentenceCase(string);
 }
 
 function selectUser (id) {
@@ -78,23 +76,37 @@ function selectUser (id) {
             <img src="${users[id].picture.large}">
             <h4>${sentenceCase(users[id].name.first)} ${sentenceCase(users[id].name.last)}</h4>
             <h5>DOB: ${users[id].dob.date}</h5>
-          </div>
-          <div class="content__main-info-contact">
+        </div>
+        <div class="content__main-info-contact">
             <ul>
-              <li>
-                <div>City</div>
-                <div>${sentenceCase(users[id].location.city)}</div>
-              </li>
-              <li>
-                <div>Cell</div>
-                <div>${sentenceCase(users[id].cell)}</div>
-              </li>
-              <li>
-                <div>E-mail</div>
-                <div>${sentenceCase(users[id].email)}</div>
-              </li>
+                <li>
+                    <div>City</div>
+                    <div>${sentenceCase(users[id].location.city)}</div>
+                </li>
+                <li>
+                    <div>Cell</div>
+                    <div>${sentenceCase(users[id].cell)}</div>
+                </li>
+                <li>
+                    <div>E-mail</div>
+                    <div>${sentenceCase(users[id].email)}</div>
+                </li>
             </ul>
-          </div>`
+        </div>`;
+}
+
+function animateUser() {
+    mainInfo.animate(
+        [
+            { transform: 'translateX(100%)', opacity:0 }, 
+            { transform: 'translateX(0)', opacity:1 }
+        ], 
+        { 
+            duration: 600,
+            iterations: 1,
+            easing: 'cubic-bezier(0.42, 0, 0.58, 1)'
+        }
+    );
 }
 
 // convert strings to sentence case
